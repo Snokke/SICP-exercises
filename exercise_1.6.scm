31365892eb36aab9fdea0f49a25519c42d5ac9e0
Andrey Babintsev <snokke@Andreys-MacBook-Pro.local>
2018-07-01 23:09:48 +0300

    add exercise 1.6

---
+++ exercise_1.6.scm
@@ -0,0 +1,39 @@
+;;;Exercise 1.6
+;Result: sqrt-new-if doesn't work: "Aborting!: maximum recursion depth exceeded"
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x)
+                   x)))
+
+(define (sqrt x)
+    (sqrt-iter 1.0 x))
+
+(sqrt 25)
+
+;custom if
+
+(define (new-if predicate then-clause else-clause)
+    (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter-new-if guess x)
+    (new-if (good-enough? guess x)
+        guess
+        (sqrt-iter-new-if (improve guess x)
+                   x)))
+
+(define (sqrt-new-if x)
+    (sqrt-iter-new-if 1.0 x))
+
+(sqrt-new-if 25)
